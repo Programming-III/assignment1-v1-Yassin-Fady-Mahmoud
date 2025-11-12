61a35ef0675a8c611e18b5a82526eae84dc729b0
Yassin Fady Mahmoud <yassinfady12@gmail.com>
2025-11-12 15:10:12 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,14 +1,45 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 
-#include <string>
-using namespace std;
 
-//define visitor class here
+class visitor{
+    private:
+     string visitorName;
+     int ticketsBought;
+
+    public:
+     visitor(){
+        visitorName="";
+        ticketsBought=0;
+     };
+
+     visitor(string visitorName , int ticketsBought){
+        this -> visitorName = visitorName;
+        this -> ticketsBought = ticketsBought;
+     };
+
+     ~visitor(){};
+
+     string getName(){
+        this -> visitorName = visitorName;
+     };
+
+     void setName(){
+        return visitorName;
+
+     };
+
+     int getTickets(){
+        this -> ticketsBought=ticketsBought;
+     };
+     void getTickets(){
+        return ticketsBought;
+     };
 
 
+     void displayInfo(); 
 
-
+};
 
 
 
